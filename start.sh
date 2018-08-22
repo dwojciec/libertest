a727945b4b150eca1fe7ec971aad8e637e64a7ff
wojciechowski <dwojciec@redhat.com>
2018-08-22 08:38:05 +0200

    8 commit

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo su - postgres -c "pgsvr -s pgdefault -y start"
-sudo -su - tester  -c "ltpconfigure.sh" 
-sudo -su - tester  -c "ltp start"
+sudo su - tester  -c "ltpconfigure.sh" 
+sudo su - tester  -c "ltp start"
